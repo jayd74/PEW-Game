93f4e289c2719696bb04a2692d5b9a57dafb16e8
Jason Du <jayd74@gmail.com>
2018-02-06 23:14:47 -0500

    score, bullet js functioning targets work in progress

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,25 +9,56 @@ $(function(){
 // on click the target disappears
 
 // set varibles with default values.
-const player = {
-    score: 0,
-    bullet: 6,
-};
+// score start at 0;
+// start with and max 6 bullets .
+    const player = {
+        score: 0,
+        bullet: 6,
+    };
+    $('.scoreCount').html(`${player.score}`);
+    $('.bulletCount').html(`${player.bullet}`);
+
 
     $('.target').on('click', function(){
         console.log(`target clicked`);
         $(this).hide();
+
+        // when click on target score + 1
         player.score ++;
-        $('.scoreCount').append(`${player.score}`);
+        $('.scoreCount').html(`${player.score}`);
+
+        // if target is clicked and there are no bullets. score does not go up (score -1 ???) and prompt reload!
+ 
+    });
+    
+    // when click bullet -1. 
+    $('body').on('click', function(){
+        // console.log('click')
+        player.bullet --;
+        $('.bulletCount').html(`${player.bullet}`);
+        
+    // if bullet is 0 prompt "no bullets, reload!"
+        if(player.bullet === 0) {
+            player.bullet ++;
+            $('.reload-prompt').show();
+            console.log('no bullets, Reload!');
+        }
     });
 
+    // if hit reload button. bullets reset to 6.
+    $('.reload-btn').on('click',function(e){
+        e.preventDefault();
+        player.bullet = 6 + 1; 
+        $('.reload-prompt').hide();
+
+        // + 1 is to make up for the 1 that is reduced during when the button is clicked
+
+        // console.log('button pressed')
+    }); 
+        
     // console.log(score);
 
-// when click on target score + 1
-// when click bullet -1. 
-// max 6 bullets.
 // if bullets = 0 and click health - 10 and prompt no bullets.
-// if hit reload button. bullets reset to 6.
 
 // once you get to 10 pts prompt you win
 
